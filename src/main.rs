f1d5c92c524cde9f55c3b0f2a7a3cb1a9314c126
Yegor Kondratyev <evkondratyev@yandex.ru>
2020-04-28 18:24:06 +0400

    Added a system for moving paddles

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use amethyst::{
     core::transform::TransformBundle,
+    input::{InputBundle, StringBindings},
     prelude::*,
     renderer::{
         plugins::{RenderFlat2D, RenderToWindow},
@@ -10,6 +11,7 @@ use amethyst::{
 };
 
 mod pong;
+mod systems;
 
 use crate::pong::Pong;
 
@@ -17,19 +19,23 @@ fn main() -> amethyst::Result<()> {
     amethyst::start_logger(Default::default());
     
     let app_root = application_root_dir()?;
-
-    let display_config_path = app_root.join("config").join("display.ron");
+    let config_root = app_root.join("config");
 
     let game_data = GameDataBuilder::default()
         .with_bundle(
             RenderingBundle::<DefaultBackend>::new()
                 .with_plugin(
-                    RenderToWindow::from_config_path(display_config_path)?
+                    RenderToWindow::from_config_path(config_root.join("display.ron"))?
                         .with_clear([0.0, 0.0, 0.0, 1.0]),
                 )
                 .with_plugin(RenderFlat2D::default()),
         )?
-        .with_bundle(TransformBundle::new())?;
+        .with_bundle(TransformBundle::new())?
+        .with_bundle(
+            InputBundle::<StringBindings>::new()
+                .with_bindings_from_file(config_root.join("bindings.ron"))?
+        )?
+        .with(systems::PaddleSystem, "paddle_system", &["input_system"]);
     
     let assets_dir = app_root.join("assets");
     
